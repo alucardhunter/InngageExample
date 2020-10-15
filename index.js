9c304bbedc802dc0d02680166febd4e83ded98df
Alucard <spdauditoria@gmail.com>
2020-10-15 10:39:31 -0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * @format
+ */
+
+import { AppRegistry } from 'react-native';
+import App from './App';
+import { name as appName } from './app.json';
+import Inngage from '@inngageregistry/inngage-react'
+
+const props = {
+  appToken: '316abc26e9dcb2a4b93ac633e461e22f'
+}
+Inngage.GetPermission(props)
+AppRegistry.registerComponent(appName, () => App);
